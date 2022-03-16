60971bfef86121e5eb8b521613bff3732b8daffd
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-03-16 23:55:20 +0500

    Added babel

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <Navbar/>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Navbar />
+        <Header />
+        <Main />
+        <Footer />
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
-const Navbar = () => {
-  return (
-    <nav>
-      <div>
-        <h3>Store Brand</h3>
-        <a href="/">Link</a>
-        <a href="/">Link</a>
-      </div>
-      <div>
+const Navbar = () => (
+  <nav>
+    <div>
+      <h3>Store Brand</h3>
+      <a href="/">Link</a>
+      <a href="/">Link</a>
+    </div>
+    <div>
       <a href="/">Link</a>
       <a href="/">Link</a>
       <a href="/">Link</a>
-      </div>
-    </nav>
-  )
-}
+    </div>
+  </nav>
+);
 
 export default Navbar;
